60f9dd28b40858128d5e126e3af562d30af67931
Arjang <arjangch@gmail.com>
2025-02-16 14:38:56 -0500

    update

--- example1/grails-app/controllers/com/arjang/EmployeeController.groovy
+++ example1/grails-app/controllers/com/arjang/EmployeeController.groovy
@@ -17,25 +17,25 @@ class EmployeeController {
 
     def printMemInfo() {
         int mb = 1024 * 1024;
-
+        def message = "hellow there".indexOf("w")
         // get Runtime instance
         Runtime instance = Runtime.getRuntime();
-
         System.out.println("***** Heap utilization statistics [MB] *****\n");
 
         // available memory
-        System.out.println("Total Memory: " + instance.totalMemory() / mb);
+        log.info("Available Memory: " + instance.availableProcessors());
+        log.info("Total Memory: " + instance.totalMemory() / mb);
 
         // free memory
-        System.out.println("Free Memory: " + instance.freeMemory() / mb);
+        log.info("Free Memory: " + instance.freeMemory() / mb);
 
         // used memory
-        System.out.println("Used Memory: "
+        log.info("Used Memory: "
                 + (instance.totalMemory() - instance.freeMemory()) / mb);
 
         // Maximum available memory
-        System.out.println("Max Memory: " + instance.maxMemory() / mb);
-
+        log.info("Max Memory: " + instance.maxMemory() / mb);
+        
         redirect(uri: "/")
     }
 
