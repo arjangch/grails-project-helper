024e3b91a5fb95333dac9604473f35af3694c37c
Arjang <arjangch@gmail.com>
2025-10-25 17:48:10 -0400

    add with BootStrap

--- example2/grails-app/init/example2/BootStrap.groovy
+++ example2/grails-app/init/example2/BootStrap.groovy
@@ -1,12 +1,18 @@
 package example2
 
 import jakarta.servlet.ServletContext
+import com.arjang.Employee
+import com.arjang.Students
 
 class BootStrap {
 
     ServletContext servletContext
 
     def init = {
+        new Employee(name: "joe", lastname: "london", empoyeeId: 12345, employeeNotes: "good guy").save()
+        new Employee(name: "Jim", lastname: "Paris", empoyeeId: 444, employeeNotes: "he is tall").save()
+        new Students(name: "jill", lastname: "green", studentId: 1212).save()
+        new Students(name: "Pam", lastname: "king", studentId: 3322).save()
     }
 
     def destroy = {
