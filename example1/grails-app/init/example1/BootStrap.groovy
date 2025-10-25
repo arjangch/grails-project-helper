4a395d718ea9629dc26966087274cbe43c20e4f1
Arjang <arjangch@gmail.com>
2025-10-25 17:40:22 -0400

    BootStrap

--- example1/grails-app/init/example1/BootStrap.groovy
+++ example1/grails-app/init/example1/BootStrap.groovy
@@ -1,8 +1,15 @@
 package example1
 
+import com.arjang.Employee
+import com.arjang.Students
+
 class BootStrap {
 
     def init = {
+        new Employee(name: "joe", lastname: "london", empoyeeId: 12345, employeeNotes: "good guy").save()
+        new Employee(name: "Jim", lastname: "Paris", empoyeeId: 444, employeeNotes: "he is tall").save()
+        new Students(name: "jill", lastname: "green", studentId: 1212).save()
+        new Students(name: "Pam", lastname: "king", studentId: 3322).save()
     }
 
     def destroy = {
